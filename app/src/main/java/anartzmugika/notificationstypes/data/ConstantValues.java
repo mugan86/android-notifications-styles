19f492185b5f7a7f07c45deac3a88c9ee8ffd91d
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-02 10:21:52 +0100

    Add constant values to app, start with sqlite database tables and columns

---
+++ app/src/main/java/anartzmugika/notificationstypes/data/ConstantValues.java
@@ -0,0 +1,30 @@
+package anartzmugika.notificationstypes.data;
+
+/**
+ * Created by anartzmugika on 2/1/17.
+ */
+
+public class ConstantValues {
+
+    //Table
+    public static final String NOTIFICATION_TABLE = "notification";
+    public static final String CONTENT_TABLE = "content";
+
+    //General columns
+    public static final String COLUMN_ID = "_id";
+
+    //Notification columns
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CONTEXT_TEXT = "context_text";
+    public static final String COLUMN_ICON = "icon";
+
+    //Message content
+
+    public static final String COLUMN_MSG_IMAGE = "image";
+    public static final String COLUMN_MSG_AUTHOR = "author";
+    public static final String COLUMN_MSG_AUTHOR_IMG = "author_img";
+    public static final String COLUMN_MSG_SOURCE = "source";
+    public static final String COLUMN_MSG_BODY = "body";
+    public static final String COLUMN_MSG_DATE = "date";
+
+}
