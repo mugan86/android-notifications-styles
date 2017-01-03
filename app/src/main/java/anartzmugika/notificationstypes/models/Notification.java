0fd5873c094d1ad3b70dbf7643030cf9ed4579a7
Anartz Muxika Ledo <mugan86@gmail.com>
2017-01-03 10:50:32 +0100

    Add clear task

--- app/src/main/java/anartzmugika/notificationstypes/models/Notification.java
+++ app/src/main/java/anartzmugika/notificationstypes/models/Notification.java
@@ -83,6 +83,7 @@ public class Notification {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // if you need to recreate activity stack
         intent.putExtra("notificationId", id);
         return intent;
     }
